ef217abebe1c73d941781d32149c60ffe27e6f3c
Stefan Piernikarczyk <spierni@gmail.com>
2020-05-14 16:47:18 +0200

    Versionsauswahl

--- installer.bat
+++ installer.bat
@@ -15,20 +15,51 @@ pause
 ECHO ADB-Geraete:
 .\ressources\adb.exe devices -l
 
+ECHO.
 ECHO Sichere APK von Geraet
 for /f %%i in ('.\ressources\adb.exe shell pm path com.huawei.KoBackup') do Set pfad=%%i
 set pfad=%pfad:~8%
 .\ressources\adb.exe pull %pfad% KoBackup.apk.backup
 
+:VERSIONASK
+ECHO.
+set /P askVersion="Welche Version von wollen sie installieren? (8/9/10)"
+ECHO.
+IF %askVersion% == 8 GOTO EIGHT
+	IF %askVersion% == 9 GOTO NINE
+		IF %askVersion% == 10 GOTO TEN
+ECHO Falsche Eingabe.
+ECHO. 
+GOTO VERSIONASK
+
 ECHO Entferne APK von Geraet
 .\ressources\adb.exe uninstall com.huawei.KoBackup
 ECHO.
-ECHO Installiere alte Version von Huawei Backup
-.\ressources\adb.exe install .\ressources\backup.apk
+
+:EIGHT
+ECHO Installiere Version 8 von Huawei Backup
+.\ressources\adb.exe install .\ressources\backup8.apk
 ECHO.
 ECHO Backup App ersetzt
+GOTO ENDE
 
-ECHO Bitte Backup auf dem Handy durchfuehren und alle .tar entpacken
+:NINE
+ECHO Installiere Version 9 von Huawei Backup
+.\ressources\adb.exe install .\ressources\backup9.apk
+ECHO.
+ECHO Backup App ersetzt
+GOTO ENDE
+
+:TEN
+ECHO Installiere Version 10 von Huawei Backup
+.\ressources\adb.exe install .\ressources\backup10.apk
+ECHO.
+ECHO Backup App ersetzt
+GOTO ENDE
+
+
+:ENDE
+ECHO Bitte Backup auf dem Handy durchfuehren.
 ECHO.
 ECHO FERTIG
 
